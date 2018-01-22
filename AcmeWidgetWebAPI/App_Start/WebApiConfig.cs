7fe1a675dc38fe25d4dac5461b4a1f4a720f9f5a
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-21 21:03:44 -0500

    added code to enable json type

--- AcmeWidgetWebAPI/App_Start/WebApiConfig.cs
+++ AcmeWidgetWebAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace AcmeWidgetWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"));
         }
     }
 }
